c63cbfc4348e2d22fdca7d0ce1adb48a7aa0ac3f
EstebanGS13 <49171835+EstebanGS13@users.noreply.github.com>
2021-03-05 21:38:50 -0500

    Move common functions to a header file

--- MM_paralela.c
+++ MM_paralela.c
@@ -1,42 +1,12 @@
-#include <processthreadsapi.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+#include "matrix_functions.h"
+
 int n, num_threads;
 int **A, **B, **C;
-
-int **init_matrix(int n, int fill) {
-    // Crea un puntero a punteros (por cada fila)
-    int **M = malloc(n * sizeof(int *));
-
-    int i, j;
-    for (i = 0; i < n; i++) {
-        // Asigna memoria para las columnas de cada fila
-        M[i] = malloc(n * sizeof(int *));
-
-        // Si hay que llenar la matriz
-        if (fill) {
-            for (j = 0; j < n; j++) {
-                M[i][j] = 1 + rand() % 10;
-            }
-        }
-    }
-    return M;
-}
-
-void print_matrix(int n, int **M) {
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d  ", M[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    printf("\n");
-}
 
 void *multiplication(void *arg) {
     int th_id, parts, start_row, end_row, i, j, k;
@@ -55,30 +25,6 @@ void *multiplication(void *arg) {
         }
     }
     pthread_exit(NULL);
-}
-
-double get_cpu_time() {
-    FILETIME a, b, c, d;
-    if (GetProcessTimes(GetCurrentProcess(), &a, &b, &c, &d) != 0) {
-        //  Returns total user time.
-        //  Can be tweaked to include kernel times as well.
-        return (double)(d.dwLowDateTime |
-                        ((unsigned long long)d.dwHighDateTime << 32)) *
-               0.0000001;
-    } else {
-        //  Handle error
-        printf("Error while calculating CPU time.");
-        return 0;
-    }
-}
-
-void free_memory(int n, int **M) {
-    int i;
-    for (i = 0; i < n; i++) {
-        free(M[i]);
-    }
-    free(M);
-    M = NULL;
 }
 
 int main(int argc, char const *argv[]) {
--- MM_secuencial.c
+++ MM_secuencial.c
@@ -1,38 +1,9 @@
-#include <processthreadsapi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int **init_matrix(int n, int fill) {
-    // Crea un puntero a punteros (por cada fila)
-    int **M = malloc(n * sizeof(int *));
 
-    int i, j;
-    for (i = 0; i < n; i++) {
-        // Asigna memoria para las columnas de cada fila
-        M[i] = malloc(n * sizeof(int *));
-
-        // Si hay que llenar la matriz
-        if (fill) {
-            for (j = 0; j < n; j++) {
-                M[i][j] = 1 + rand() % 10;
-            }
-        }
-    }
-    return M;
-}
-
-void print_matrix(int n, int **M) {
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d  ", M[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    printf("\n");
-}
+#include "matrix_functions.h"
 
 void multiplication(int n, int **A, int **B, int **C) {
     int i, j, k;
@@ -44,28 +15,6 @@ void multiplication(int n, int **A, int **B, int **C) {
             }
         }
     }
-}
-
-double get_cpu_time() {
-    FILETIME a, b, c, d;
-    if (GetProcessTimes(GetCurrentProcess(), &a, &b, &c, &d) != 0) {
-        //  Returns total user time.
-        //  Can be tweaked to include kernel times as well.
-        return (double)(d.dwLowDateTime | ((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001;
-    } else {
-        //  Handle error
-        printf("Error while calculating CPU time.");
-        return 0;
-    }
-}
-
-void free_memory(int n, int **M) {
-    int i;
-    for (i = 0; i < n; i++) {
-        free(M[i]);
-    }
-    free(M);
-    M = NULL;
 }
 
 int main(int argc, char const *argv[]) {
