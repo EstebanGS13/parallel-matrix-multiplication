4024b8ec3908052f8a9b9d835b1ea3b236326bcc
EstebanGS13 <49171835+EstebanGS13@users.noreply.github.com>
2021-03-22 19:03:29 -0500

    Add code to handle matrix multiplication on multiple processes

--- processes/parallel.c
+++ processes/parallel.c
@@ -1,70 +1,113 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "matrix_functions.h"
 
-int n, num_threads;
-int **A, **B, **C;
-
-void *multiplication(void *arg) {
-    int th_id, parts, start_row, end_row, i, j, k;
-    th_id = *((int *)arg);
-
-    parts = n / num_threads;
-    start_row = parts * th_id;
-    end_row = parts * (th_id + 1);
-
+void multiplication(int n, int start_row, int end_row, int **A, int **B, int *C) {
+    int i, j, k, index;
     for (i = start_row; i < end_row; i++) {
         for (j = 0; j < n; j++) {
+            index = n * i + j;
+            C[index] = 0;
             for (k = 0; k < n; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[index] += A[i][k] * B[k][j];
             }
         }
     }
-    pthread_exit(NULL);
+}
+
+void print_C(int n, int *C) {
+    int i, j, index;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            index = n * i + j;
+            printf("%d  ", C[index]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 int main(int argc, char const *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Error. Entregar el 'n' y el numero de hilos.\nEjemplo: MM_paralela 400 4");
+        fprintf(stderr, "Error. Entregar el 'n' y el numero de procesos.\nEjemplo: parallel 400 4");
         return -1;
     }
 
     srand(time(NULL));
-    n = atoi(argv[1]);
-    num_threads = atoi(argv[2]);
 
-    if (n % num_threads != 0) {
-        fprintf(stderr, "La división de 'n' entre el numero de hilos debe ser exacta");
+    // Convertir los datos entregados por consola
+    int n = atoi(argv[1]);
+    int num_processes = atoi(argv[2]);
+    if (n % num_processes != 0) {
+        fprintf(stderr, "La división de 'n' entre el numero de procesos debe ser exacta");
         return -1;
     }
+    int parts = n / num_processes;  // Partes en que serán divididas las filas de la matriz C
 
     // Inicializar las matrices
-    A = init_matrix(n, 1);
-    B = init_matrix(n, 1);
-    C = init_matrix(n, 0);
+    int **A = init_matrix(n, 1);
+    int **B = init_matrix(n, 1);
+    // int **C = init_matrix(n, 0);
 
-    // Crear los hilos
-    pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
-    pthread_attr_t attribute;
-    pthread_attr_init(&attribute);
-    pthread_attr_setdetachstate(&attribute, PTHREAD_CREATE_JOINABLE);
+    // 1. Crear segmento de memoria compartida
+    // TODO: verificar sizeof
+    int segment = shmget(IPC_PRIVATE, (n * n * sizeof(int)), IPC_CREAT | 0666);
+    if (segment < 0) {
+        perror("shmget error");
+        exit(-1);
+    }
+    printf("Allocated %d, at id %d\n", (int)(n * n * sizeof(int)), segment);
+
+    // 2. Crear los procesos hijos
+    int parent_id = getpid();
+    int process;
 
     // Comenzar a medir el tiempo
     double begin = get_cpu_time();
 
-    // Calcular multiplicación
-    for (int i = 0; i < num_threads; i++) {
-        int *th_id = malloc(sizeof(int));
-        *th_id = i;
-        pthread_create(&threads[i], &attribute, multiplication, (void *)th_id);
+    for (int i = 0; i < num_processes; i++) {
+        if (fork() == 0) {  // Si se ejecuta el proceso hijo
+            process = i;
+            break;
+        }
     }
 
-    // Unir los hilos
-    for (int i = 0; i < num_threads; ++i) {
-        pthread_join(threads[i], NULL);
+    // 3. Adjuntar el segmento a los procesos
+    int *shared_C = shmat(segment, NULL, 0);
+    if (shared_C == (void *)-1) {
+        perror("shmat error");
+        exit(-1);
+    }
+    printf("Process %d attached the segment %d\n", getpid(), segment);
+
+    if (getpid() != parent_id) {  // Se ejecuta un proceso hijo
+        // 4. Hacer la multiplicación
+        int start_row = parts * process;
+        int end_row = parts * (process + 1);
+        multiplication(n, start_row, end_row, A, B, shared_C);
+
+        // 5. Separar el segmento del proceso hijo
+        int detach = shmdt(shared_C);
+        if (detach < 0) {
+            perror("shmdt error");
+            exit(-1);
+        }
+        printf("Process %d detached the segment %d\n", getpid(), segment);
+
+        // 6. Terminar el proceso
+        exit(0);
+
+    } else {
+        // Se ejecuta el proceso padre
+        for (int i = 0; i < num_processes; i++) {
+            // Esperar a que los procesos terminen
+            wait(NULL);
+        }
     }
 
     // Detener la medición del tiempo y calcular el tiempo transcurrido
@@ -78,26 +121,37 @@ int main(int argc, char const *argv[]) {
         printf("Matriz B:\n");
         print_matrix(n, B);
         printf("Matriz C:\n");
-        print_matrix(n, C);
+        print_C(n, shared_C);
     }
     printf("Time measured: %.3f seconds.\n", elapsed);
 
-    pthread_attr_destroy(&attribute);
+    // Escribir resultados en un archivo
+    FILE *file = fopen("elapsed_par_proc.csv", "a");
+    if (file == NULL) {
+        printf("No se puede abrir elapsed_par_proc.csv.csv");
+        return -1;
+    }
+    fprintf(file, "%d, %d, %f\n", num_processes, n, elapsed);
+    fclose(file);
+
+    // Separar el segmento del proceso padre
+    int detach = shmdt(shared_C);
+    if (detach < 0) {
+        perror("shmdt error");
+        exit(-1);
+    }
+    printf("Process %d detached the segment %d\n", getpid(), segment);
+
+    // 7. Borrar el segmento
+    int delete = shmctl(segment, IPC_RMID, NULL);
+    if (delete < 0) {
+        perror("shmctl error");
+        exit(-1);
+    }
 
     // Liberar la memoria usada
     free_memory(n, A);
     free_memory(n, B);
-    free_memory(n, C);
 
-    // Escribir resultados en un archivo
-    FILE *file = fopen("elapsed_par.csv", "a");
-    if (file == NULL) {
-        printf("No se puede abrir elapsed_par.csv");
-        return -1;
-    }
-    fprintf(file, "%d, %d, %f\n", num_threads, n, elapsed);
-    fclose(file);
-
-    pthread_exit(NULL);
     return 0;
 }
